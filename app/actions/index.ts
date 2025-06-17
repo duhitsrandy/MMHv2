b638ff7c139a2a03ba7e8e3a1776460de3d6da51
duhitsrandy <duhitsrandy@gmail.com>
2025-06-16 20:55:49 -0400

    Fix deployment: Update locations-actions export path in app/actions/index.ts

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,7 +1,7 @@
 export { getTrafficMatrixHereAction } from './here-actions';
 export * from './user-actions';
 export * from './locationiq-actions';
-export * from './db/locations-actions';
+export * from '../../actions/db/locations-actions';
 
 // If you add other action files directly in app/actions later,
 // you can re-export their named exports here too, for example:
